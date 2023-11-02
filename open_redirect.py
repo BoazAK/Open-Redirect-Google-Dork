55a3ba1347d8162a1b7d60a4adae143a9b4a1b38
KodjoDoDjango <kodjododjango@MacBook-Pro-de-Kodjo.local>
2023-11-02 17:41:10 +0000

    Start writing the code for Google search

--- open_redirect.py
+++ open_redirect.py
@@ -1,3 +1,22 @@
-site = input("Example :\ngoogle.com\nor\n*.google.com\n\nEnter website:")
-print("The website is: " + site)
-print("site:" + site)
+print("*****" * 15)
+print("-----" * 15)
+print("\tBy Boaz Kodjo BoazAK, aka KodjoDoDjango, aka InfoSecKodjo")
+print("\t\t\thttps://twitter.com/BoazakK")
+print("-----" * 15)
+print("*****" * 15)
+
+# Take the input from user
+site = input("\n\nExample :\ngoogle.com\nor\n*.google.com\n\nEnter website:")
+# Show the input in terminal
+print("The website you're looking fo is: " + site + "\n\n")
+
+# Read the file containing the Google Dorks keywords
+file1 = open('dork.txt', 'r')
+Lines = file1.readlines()
+
+# Strips the newline character
+for line in Lines:
+    print("site:{}: {}".format(site, line.strip()))
+
+
+    # Search with Google
